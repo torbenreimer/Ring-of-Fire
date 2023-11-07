25ca4c4a00a175382a7e5b617ad19de39679dab7
Torben Luca Reimer <torben.luca.reimer@gmail.com>
2023-11-07 21:21:38 +0100

    animate pick-card

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Game } from 'src/models/game';
 
 @Component({
   selector: 'app-game',
@@ -7,14 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameComponent implements OnInit {
   pickCardAnimation = false;
-  
-  constructor() {  }
+  game: Game;
+  currentCard: String;
+
+  constructor() { }
 
   ngOnInit(): void {
+    this.newGame();
+  }
 
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
   }
 
   takeCard() {
-    this.pickCardAnimation = true;
+    if (!this.pickCardAnimation) {
+      this.currentCard = this.game.stack.pop()!;
+      this.pickCardAnimation = true;
+      setTimeout(() => {
+        this.pickCardAnimation = false;
+      }, 2000);
+
+      this.playedCard();
+    }
   }
+
+  playedCard() {
+    this.game.playedCard.push(this.currentCard);
+  }
+
 }
